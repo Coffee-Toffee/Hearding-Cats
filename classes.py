78f9bd28364bbf0c63eca1434e6f872127d86ee0
Luna Harrison <65498482+Coffee-Toffee@users.noreply.github.com>
2023-01-30 23:07:15 -0500

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,14 +1,34 @@
 import pygame as pg
 import random
 
-class Player(pg.sprite.Sprite):
+
+class Sprite(pg.sprite.Sprite):
     def __init__(self):
+        self.strength = 0 #str pos for an attractor, neg for detractor
         pg.sprite.Sprite.__init__(self)
         self.pos = (0,0)
-    
+
+    def set_strength(self, strength):
+        self.strength = strength 
+
+    def get_strength(self):
+        return(self.strength)
+
     def set_image(self, Sprite):
         self.image = pg.image.load(Sprite)
         self.rect = self.image.get_rect()
+
+    def set_pos(self,pos):
+        self.pos = pos
+
+
+class Player(Sprite):
+    '''
+    Todo: actual movement system
+    '''
+    def __init__(self, strength): 
+        Sprite.__init__(self)
+        self.set_strength(strength)
 
     def forward(self, speed):
         self.pos = (self.pos[0], self.pos[1]-speed*0.1)
@@ -19,41 +39,46 @@ class Player(pg.sprite.Sprite):
     def right(self, speed):
         self.pos = (self.pos[0]+speed*0.1, self.pos[1])
    
-
-
-    def set_pos(self,pos):
-        self.pos = pos
-
     def update(self):
-        #self.pos = pg.mouse.get_pos()
-        #self.pos = list(map(lambda x: int(x), self.pos))
         self.rect.topleft = list(map(lambda x: int(x+50), self.pos))
- 
 
-class Cat(pg.sprite.Sprite):
-    def __init__(self):
-        pg.sprite.Sprite.__init__(self)
-        self.pos = (0,0)
-        self.i=0
-    def set_image(self, Sprite):
-        self.image = pg.image.load(Sprite)
-        self.rect = self.image.get_rect()
 
-    def wander(self, move, sleepy):
+class Static(Sprite):
+    '''
+    For anything that doesn't move, eg. fences, powerups, the corral
+
+    collision system eventualy, but is decent framework
+    '''
+    def __init__(self, strength, pos):
+        Sprite.__init__(self)
+        self.set_strength(strength)
+        self.set_pos(pos)
+        self.shown = 0
+
+    def show(self):
+        self.rect.topleft = list(map(lambda x: int(x), self.pos))
+    def update(self):
+        if not self.shown:
+            self.show() 
+            self.shown = 1
+
+class Cat(Sprite):
+    def wander(self, move, sleep_time):
+        '''
+        Todo: implement a movement system for the cats
+        Future ideas:
+        different types of cats, with different patterns
+        of behaviors, so that way it's more interesting
+        '''
         if (move):
-            pg.time.wait(sleepy)
+            pg.time.wait(sleep_time)
             pos = ((self.pos[0] + random.randrange(-5, 5)), (self.pos[1] + random.randrange(-5, 5)))
         else: 
             pos = self.pos
         return(pos)
-    
-    def set_pos(self,pos):
-        self.pos = pos
 
     def update(self):
-        #self.pos = pg.mouse.get_pos()
         #self.pos = self.wander(True, 350)  
-        #self.pos = list(map(lambda x: (x+1), self.pos))
+        self.pos = list(map(lambda x: (x+0.1*random.random()), self.pos))
         self.rect.topleft = list(map(lambda x: int(x), self.pos))
-        #self.i+=1
         pass
--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import sys, random 
 import pygame as pg
 from classes import * 
+from threading import Thread
+
 pg.init()
 
 size = width, height =1000 , 1400
@@ -8,45 +10,38 @@ speed = [2,-1]
 black = 0,0,0
 dgreen = 6,70,24
 
-modulus = 10
+player_speed = 3.3
+#modulus = 10
 
 screen = pg.display.set_mode(size)
-#cat = pygame.image.load("cat.png")
-#catrect = cat.get_rect()
+
 loop = 0
+
 cat = Cat()
 cat.set_image("cat.png")
 
-player = Player()
+player = Player(-1)
 player.set_image("catgirl.png")
 player.set_pos((250,250))
-speed = 3.3
 
-allsprites = pg.sprite.RenderPlain(cat, player)
+test = Static(-1, (100,100))
+test.set_image("cat.png")
+
+
+allsprites = pg.sprite.RenderPlain(cat, player, test)
+
 while True:
 
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
-        
+    
+    speed = player_speed
     if pg.key.get_pressed()[pg.K_w] : player.forward(speed)
     if pg.key.get_pressed()[pg.K_a] : player.left(speed)
     if pg.key.get_pressed()[pg.K_s] : player.backward(speed)
     if pg.key.get_pressed()[pg.K_d] : player.right(speed)
 
-        
-
-    '''
-    if loop%modulus == 0:
-        catrect = catrect.move(speed)
-        if catrect.left < 0 or catrect.right > width :
-            speed[0] = -speed[0]
-    
-    if catrect.top < 0:
-        catrect.top = height
-    '''
-
     screen.fill(dgreen)
     allsprites.update()
     allsprites.draw(screen)
     pg.display.flip()
-
